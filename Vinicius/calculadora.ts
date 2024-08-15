14bec80a14e57be44d8ceea63a731f9650860bed
vinithecsar <viniciuscesarbrito@gmail.com>
2024-08-15 11:09:42 -0300

    Adicionada classe calculadora com as 4 operações básicas da matemática

---
+++ Vinicius/calculadora.ts
@@ -0,0 +1,17 @@
+export abstract class Calculadora {
+  static somar(a: number, b: number) {
+    return a + b;
+  }
+
+  static subtrair(a: number, b: number) {
+    return a - b;
+  }
+
+  static multiplicar(a: number, b: number) {
+    return a * b;
+  }
+
+  static dividir(a: number, b: number) {
+    return a / b;
+  }
+}
